56cc5f3b1a74cf85825b7f4ab11925761f12e56c
nileshvarishe <nileshvarishe@gmail.com>
2020-07-04 21:38:07 +0530

    Updates

---
+++ precache-manifest.7c8a4aa462c5e0f1d4f7740fe4a83de8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eae85fc5098e81d38a83784932cd4a46",
+    "url": "/blazeclan/index.html"
+  },
+  {
+    "revision": "e4b0b368a446bee8fea3",
+    "url": "/blazeclan/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3e49b38240e729b81ee5",
+    "url": "/blazeclan/static/js/2.17bfb227.chunk.js"
+  },
+  {
+    "revision": "253d0d72fd1c6eb1e9606c0cb98dca11",
+    "url": "/blazeclan/static/js/2.17bfb227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4b0b368a446bee8fea3",
+    "url": "/blazeclan/static/js/main.56ac73ac.chunk.js"
+  },
+  {
+    "revision": "d1750732385fe970013e",
+    "url": "/blazeclan/static/js/runtime-main.cb3ce3c3.js"
+  }
+]);
